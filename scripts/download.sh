6a0be75a220d359c819d80a24e6a540e8ebd6727
root <edward>
2015-01-12 11:11:06 -0800

    changing scripts

---
+++ scripts/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # first error should stop execution of this script
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -f *.zip
+wget -v http://downloads.typesafe.com/typesafe-activator/$ACTIVATORVER/typesafe-activator-$ACTIVATORVER-minimal.zip
+unzip -oq typesafe-activator-$ACTIVATORVER-minimal.zip
+echo ${BASE_DIR}
+mv -f activator-$ACTIVATORVER-minimal/* . && rm -rf activator-$ACTIVATORVER-minimal
+#rm -f *.zip
+set +e
