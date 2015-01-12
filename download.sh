d4aa4bb02d3b393a33a797976a1656293c813b98
root <edward>
2015-01-12 11:33:33 -0800

    try to install activator

--- download.sh
+++ download.sh
@@ -3,7 +3,8 @@ set -e # first error should stop execution of this script
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -f *.zip
 wget -v http://downloads.typesafe.com/typesafe-activator/$ACTIVATORVER/typesafe-activator-$ACTIVATORVER-minimal.zip
-unzip -q typesafe-activator-$ACTIVATORVER-minimal.zip
-mv $BASE_DIR/activator-$ACTIVATORVER-minimal/* . && rm -rf activator-$ACTIVATORVER-minimal
-rm -f *.zip
+unzip -oq typesafe-activator-$ACTIVATORVER-minimal.zip
+echo ${BASE_DIR}
+mv -f $BASE_DIR/activator-$ACTIVATORVER-minimal/* . && rm -rf activator-$ACTIVATORVER-minimal
+#rm -f *.zip
 set +e
